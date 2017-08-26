7fa9cb78febc3c912c97e3cad9aa30150d709cb7
Stephen Maxwell <stephen@stephenmaxwell.me>
2017-08-26 22:08:33 +0100

    E

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,11 +1,20 @@
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 (require 'package)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
 
 (add-to-list 'default-frame-alist '(font . "Iosevka-13" ))
 (set-face-attribute 'default t :font "Iosevka-13" )
 
 ;;; Disable useless GUI stuff
-(tool-bar-mode -1)
+;;(tool-bar-mode -1)
 (scroll-bar-mode -1)
 ;;(menu-bar-mode -1)
 (blink-cursor-mode -1)
@@ -15,3 +24,24 @@
 (fset #'display-startup-echo-area-message #'ignore)
 (setq inhibit-splash-screen t)
 (setq initial-scratch-message "")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (elfeed-org ## org))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Load elfeed-org
+(require 'elfeed-org)
+
+;; Initialize elfeed-org
+;; This hooks up elfeed-org to read the configuration when elfeed
+;; is started with =M-x elfeed=
+(elfeed-org)
+
