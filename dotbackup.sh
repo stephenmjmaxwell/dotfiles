2390650c881cab3186a6589b42cd23642a9bb93f
Stephen Maxwell <stephen@stephenmaxwell.me>
2017-09-01 23:32:57 +0100

    rat

--- dotbackup.sh
+++ dotbackup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-cp ~/.config/i3/config ~/dotfiles/config/i3/
 cp ~/.config/qutebrowser/qutebrowser.conf ~/dotfiles/config/qutebrowser/
 cp ~/.xinitrc ~/dotfiles/X/
 cp ~/.Xdefaults ~/dotfiles/X/
@@ -10,6 +9,8 @@ cp ~/.tmux.conf ~/dotfiles/
 cp ~/bin/* ~/dotfiles/bin/
 cp ~/.emacs.d/elfeed.org ~/Main/Doc/personal/feed/
 cp -r ~/.password-store ~/Main/Mobile/
+cp ~/.ratpoisonrc ~/dotfiles/
 
 #cp ~/.config/fish/config.fish ~/dotfiles/config/fish/
 #cp ~/.config/ranger/rc.conf ~/dotfiles/config/ranger/
+#cp ~/.config/i3/config ~/dotfiles/config/i3/
