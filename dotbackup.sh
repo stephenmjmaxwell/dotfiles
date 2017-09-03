3396c915b604171469ae2774ed0deaa8d0964346
Stephen Maxwell <stephen@stephenmaxwell.me>
2017-09-03 11:37:35 +0100

    e

--- dotbackup.sh
+++ dotbackup.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-cp ~/.config/qutebrowser/qutebrowser.conf ~/dotfiles/config/qutebrowser/
 cp ~/.xinitrc ~/dotfiles/X/
 cp ~/.Xdefaults ~/dotfiles/X/
 cp ~/.bashrc ~/dotfiles/bash/
-cp ~/.emacs.d/init.el ~/dotfiles/emacs.d/
-cp ~/.tmux.conf ~/dotfiles/
-cp ~/bin/* ~/dotfiles/bin/
-cp ~/.emacs.d/elfeed.org ~/Main/Doc/personal/feed/
 cp ~/.ratpoisonrc ~/dotfiles/
+cp ~/bin/* ~/dotfiles/bin/
+cp ~/.emacs.d/init.el ~/dotfiles/emacs.d/
+cp ~/.emacs.d/elfeed.org ~/Main/Doc/personal/feed/
+cp ~/.config/qutebrowser/qutebrowser.conf ~/dotfiles/config/qutebrowser/
 cp -r ~/.password-store ~/Main/Mobile/
-
-#cp ~/.config/fish/config.fish ~/dotfiles/config/fish/
-#cp ~/.config/ranger/rc.conf ~/dotfiles/config/ranger/
-#cp ~/.config/i3/config ~/dotfiles/config/i3/
