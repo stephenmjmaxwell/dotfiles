3f5e599c0c7206292b3ee781b1b9cac6236fe09a
Stephen Maxwell <stephen@stephenmaxwell.me>
2017-09-08 23:59:57 +0100

    w3m

--- dotbackup.sh
+++ dotbackup.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-cp ~/.xinitrc ~/dotfiles/X/
-cp ~/.Xdefaults ~/dotfiles/X/
-cp ~/.bashrc ~/dotfiles/bash/
+cp ~/.xinitrc ~/dotfiles/
+cp ~/.Xdefaults ~/dotfiles/
+cp ~/.bashrc ~/dotfiles/
 cp ~/.ratpoisonrc ~/dotfiles/
 cp ~/bin/* ~/dotfiles/bin/
 cp ~/.emacs.d/init.el ~/dotfiles/emacs.d/
 cp ~/.emacs.d/configuration.org ~/dotfiles/emacs.d/
 cp ~/.emacs.d/elfeed.org ~/Main/Doc/personal/feed/
-cp ~/.config/qutebrowser/qutebrowser.conf ~/dotfiles/config/qutebrowser/
 cp -r ~/.password-store ~/Main/Mobile/
 cp ~/.authinfo.gpg ~/Main/Doc/personal/
