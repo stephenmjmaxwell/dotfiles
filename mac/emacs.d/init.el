2e1ec26f1b6f96c6595b757e5f58e7f480b621eb
Stephen Maxwell <stephenmaxwell@Stephens-MacBook-Air.local>
2024-05-17 12:30:59 +0100

    mac

---
+++ mac/emacs.d/init.el
@@ -0,0 +1,15 @@
+(package-initialize)
+(org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(solo-jazz-theme pdf-tools treemacs org-download ## solarized-theme spacemacs-theme olivetti)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
